012c8a35e50a2a6e7144ca27d1b239c631ac5da2
Mohaek <elkasmimoha@gmail.com>
2023-11-11 18:21:13 +0100

    Initial Commit

---
+++ ApiMadMotor/src/test/java/org/madmotor/apimadmotor/ApiMadMotorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.madmotor.apimadmotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMadMotorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
