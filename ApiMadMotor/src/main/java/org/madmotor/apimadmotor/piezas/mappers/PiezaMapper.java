3495cf805ce8c19d7b2fc29d5c1c966c7f993121
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-11-12 20:50:39 +0100

    Added Repository and Service

---
+++ ApiMadMotor/src/main/java/org/madmotor/apimadmotor/piezas/mappers/PiezaMapper.java
@@ -0,0 +1,5 @@
+package org.madmotor.apimadmotor.piezas.mappers;
+
+public class PiezaMapper {
+    
+}
--- ApiMadMotor/src/main/java/org/madmotor/apimadmotor/piezas/repositories/PiezaRepository.java
+++ ApiMadMotor/src/main/java/org/madmotor/apimadmotor/piezas/repositories/PiezaRepository.java
@@ -2,9 +2,10 @@ package org.madmotor.apimadmotor.piezas.repositories;
 
 import org.madmotor.apimadmotor.piezas.models.Pieza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface PiezaRepository extends JpaRepository<Pieza, UUID> {
-    
+
 }
